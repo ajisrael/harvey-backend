f942f3ec27c3dfe953fcb893ef02e80e7385feba
aiuhjv1 <ajisrael@mtu.edu>
2022-04-19 15:39:22 -0400

    f   - added route for posting data to server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import gardenBedConfig from './esp32_node_config/gardenBedConfig.js';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   console.log('API is running...');
@@ -11,6 +14,12 @@ app.get('/', (req, res) => {
 app.get('/api/esp32config/gardenBed', (req, res) => {
   console.log('Returning garden bed config');
   res.json(gardenBedConfig);
+});
+
+app.post('/api/data/gardenBed', (req, res) => {
+  console.log('Data received from garden bed:');
+  console.log(req.body);
+  res.send('Success').status(200);
 });
 
 app.listen(5000, console.log('Server running on port 5000'));
