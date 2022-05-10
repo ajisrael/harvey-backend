3913a7339796235373eed78a231c7fcb20b2fa1c
aiuhjv1 <ajisrael@mtu.edu>
2022-05-09 22:08:21 -0400

    r   - disabled user registration through ui

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,6 +8,9 @@ import { loginUser, registerUser } from '../controllers/userController.js';
 const router = express.Router();
 
 router.route('/login').post(validateLoginData, loginUser);
-router.route('/').post(validateRegistrationData, registerUser);
+
+// User registration disabled until data structures added for
+// mapping beds to a garden and gardens to a list of users
+//router.route('/').post(validateRegistrationData, registerUser);
 
 export default router;
