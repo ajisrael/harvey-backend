b3a23cf7dc0347ace63f01d3df0bb750dcb58d1a
aiuhjv1 <ajisrael@mtu.edu>
2022-05-16 23:32:43 -0400

    t   - added feature to removeSqlFields to work with both arrays and single objects

--- test/utilities/dataCleaner.js
+++ test/utilities/dataCleaner.js
@@ -1,8 +1,13 @@
 const removeSqlFields = (dbData) => {
-  dbData.forEach((entry) => {
-    delete entry.id;
-    delete entry.created_at;
-  });
+  if (Array.isArray(dbData)) {
+    dbData.forEach((entry) => {
+      delete entry.id;
+      delete entry.created_at;
+    });
+  } else {
+    delete dbData.id;
+    delete dbData.created_at;
+  }
 
   return dbData;
 };
