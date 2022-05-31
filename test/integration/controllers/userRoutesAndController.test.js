82d9d63a3fe916b8db3297b351dae4ef3f494f0b
aiuhjv1 <ajisrael@mtu.edu>
2022-05-31 15:28:04 -0400

    t   - created route and controller test for users

---
+++ test/integration/controllers/userRoutesAndController.test.js
@@ -0,0 +1,52 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../../src/server.js';
+import { resetDB } from '../../../src/seeder.js';
+import { stubLogs, restoreLogs } from '../../utilities/testSetup.js';
+import users from '../../../src/data/userData.js';
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+const url = '/api/v1/users';
+
+const checkUsersData = (data, name, email) => {
+  data._id.should.be.a('number');
+  data.name.should.be.a('string');
+  data.email.should.be.a('string');
+  data.isAdmin.should.be.a('boolean');
+  data.token.should.be.a('string');
+  data.name.should.eql(name);
+  data.email.should.eql(email);
+};
+
+describe('Solenoid State Controller and Routes', () => {
+  before((done) => {
+    stubLogs();
+    resetDB();
+    done();
+  });
+
+  after((done) => {
+    restoreLogs();
+    done();
+  });
+
+  describe(`POST ${url}/login`, () => {
+    it('should login a user', (done) => {
+      const email = users[0].email;
+      const password = users[0].password;
+
+      chai
+        .request(server)
+        .post(`${url}/login`)
+        .send({ email, password })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          checkUsersData(res.body, users[0].name, users[0].email);
+          done();
+        });
+    });
+  });
+});
