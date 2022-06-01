3aa931f17c65b4a4396500b8745c0da40521d2b5
aiuhjv1 <ajisrael@mtu.edu>
2022-06-01 16:22:15 -0400

    F   - added stricter validation to actions

--- src/middleware/actionsValidator.js
+++ src/middleware/actionsValidator.js
@@ -1,29 +1,46 @@
+function areBedIdsStrings(bedIds) {
+  const nonStrings = bedIds.filter((bedId) => {
+    return typeof bedId !== 'string';
+  });
+  return nonStrings.length === 0;
+}
+
 function validateActionData(req, res, next) {
   let messages = [];
 
   if (!req.body) {
-    messages.push('No object is provided');
+    messages.push('body required for request');
   }
 
   if (!req.body.bedIds) {
-    messages.push('Bed IDs is empty');
+    messages.push('bedIds is empty');
+  } else if (!Array.isArray(req.body.bedIds)) {
+    messages.push('bedIds must be an array');
+  } else if (!areBedIdsStrings(req.body.bedIds)) {
+    messages.push('bedId in bedIds array must be a string');
   }
 
   if (!req.body.actionType) {
-    messages.push('Action Type is empty');
+    messages.push('actionType is empty');
+  } else if (typeof req.body.actionType !== 'string') {
+    messages.push('actionType must be a string');
   }
 
   if (!req.body.actionCompletedType) {
-    messages.push('Action Completed Type is empty');
+    messages.push('actionCompletedType is empty');
+  } else if (typeof req.body.actionCompletedType !== 'string') {
+    messages.push('actionCompletedType must be a string');
   }
 
   if (!req.body.actionCompleted) {
-    messages.push('Action Completed is empty');
+    messages.push('actionCompleted is empty');
+  } else if (typeof req.body.actionCompleted !== 'number') {
+    messages.push('actionCompleted must be a number');
   }
 
   if (messages.length) {
     let error = new Error(messages.join(', '));
-    error.statusCode = 400;
+    res.status(400);
     throw error;
   } else {
     next();
