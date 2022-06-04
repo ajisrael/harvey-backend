e1fa43e8354b8f7e63942a38973e2e7fcec70de6
aiuhjv1 <ajisrael@mtu.edu>
2022-06-04 17:06:03 -0400

    f   - added type validation to userValidator

--- src/middleware/userValidator.js
+++ src/middleware/userValidator.js
@@ -1,21 +1,25 @@
 function validateLoginData(req, res, next) {
   let messages = [];
 
-  if (!req.body) {
-    messages.push('No object is provided');
-  }
+  if (Object.keys(req.body).length === 0) {
+    messages.push('body required for request');
+  } else {
+    if (!req.body.email) {
+      messages.push('email is empty');
+    } else if (typeof req.body.email !== 'string') {
+      messages.push('email must be a string');
+    }
 
-  if (!req.body.email) {
-    messages.push('Email is empty');
-  }
-
-  if (!req.body.password) {
-    messages.push('Password is empty');
+    if (!req.body.password) {
+      messages.push('password is empty');
+    } else if (typeof req.body.password !== 'string') {
+      messages.push('password must be a string');
+    }
   }
 
   if (messages.length) {
     let error = new Error(messages.join(', '));
-    error.statusCode = 400;
+    res.status(400);
     throw error;
   } else {
     next();
@@ -25,20 +29,26 @@ function validateLoginData(req, res, next) {
 function validateRegistrationData(req, res, next) {
   let messages = [];
 
-  if (!req.body) {
-    messages.push('No object is provided');
-  }
+  if (Object.keys(req.body).length === 0 || !req.body) {
+    messages.push('body required for request');
+  } else {
+    if (!req.body.name) {
+      messages.push('name is empty');
+    } else if (typeof req.body.name !== 'string') {
+      messages.push('name must be a string');
+    }
 
-  if (!req.body.name) {
-    messages.push('Name is empty');
-  }
+    if (!req.body.email) {
+      messages.push('email is empty');
+    } else if (typeof req.body.email !== 'string') {
+      messages.push('email must be a string');
+    }
 
-  if (!req.body.email) {
-    messages.push('Email is empty');
-  }
-
-  if (!req.body.password) {
-    messages.push('Password is empty');
+    if (!req.body.password) {
+      messages.push('password is empty');
+    } else if (typeof req.body.password !== 'string') {
+      messages.push('password must be a string');
+    }
   }
 
   if (messages.length) {
