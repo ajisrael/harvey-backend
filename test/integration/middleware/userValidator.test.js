888685d4dc6e8e47589ad9a6d0c8d368f462f7f2
aiuhjv1 <ajisrael@mtu.edu>
2022-06-04 17:06:34 -0400

    t   - tests for type validation on user validator

--- test/integration/middleware/userValidator.test.js
+++ test/integration/middleware/userValidator.test.js
@@ -7,7 +7,8 @@ import {
   restoreLogs,
   getAdminUserToken,
 } from '../../utilities/testHelper.js';
-import users from '../../../src/data/userData.js';
+import serverConfig from '../../../src/config/serverConfig.js';
+import { checkValidationResponse } from '../../utilities/dataChecker.js';
 
 const should = chai.should();
 chai.use(chaiHttp);
@@ -32,57 +33,183 @@ describe('userValidator', () => {
     done();
   });
 
-  it('should NOT register a new user when missing name', (done) => {
-    const userMissingName = Object.assign({}, newUser);
-    delete userMissingName.name;
-    chai
-      .request(server)
-      .post(url)
-      .set('Authorization', `Bearer ${getAdminUserToken()}`)
-      .send(userMissingName)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.message.should.be.a('string');
-        res.body.stack.should.be.a('string');
-        res.body.message.should.eql('Name is empty');
-        done();
-      });
+  describe('validateLoginData', () => {
+    it('should NOT login a user when body is empty', (done) => {
+      chai
+        .request(server)
+        .post(`${url}/login`)
+        .send({})
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'body required for request');
+          done();
+        });
+    });
+
+    it('should NOT login a user when email is empty', (done) => {
+      const loginUser = { password: '1234567' };
+      chai
+        .request(server)
+        .post(`${url}/login`)
+        .send(loginUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'email is empty');
+          done();
+        });
+    });
+
+    it('should NOT login a user when email is not a string', (done) => {
+      const loginUser = { email: 1, password: '1234567' };
+      chai
+        .request(server)
+        .post(`${url}/login`)
+        .send(loginUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'email must be a string');
+          done();
+        });
+    });
+
+    it('should NOT login a user when password is empty', (done) => {
+      const loginUser = { email: 'test@test.com' };
+      chai
+        .request(server)
+        .post(`${url}/login`)
+        .send(loginUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'password is empty');
+          done();
+        });
+    });
+
+    it('should NOT login a user when password is not a string', (done) => {
+      const loginUser = { email: 'test@test.com', password: 1 };
+      chai
+        .request(server)
+        .post(`${url}/login`)
+        .send(loginUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'password must be a string');
+          done();
+        });
+    });
   });
 
-  it('should NOT register a new user when missing email', (done) => {
-    const userMissingEmail = Object.assign({}, newUser);
-    delete userMissingEmail.email;
-    chai
-      .request(server)
-      .post(url)
-      .set('Authorization', `Bearer ${getAdminUserToken()}`)
-      .send(userMissingEmail)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.message.should.be.a('string');
-        res.body.stack.should.be.a('string');
-        res.body.message.should.eql('Email is empty');
-        done();
-      });
-  });
+  describe('validateRegistrationData', () => {
+    it('should NOT register a new user when body is empty', (done) => {
+      chai
+        .request(server)
+        .post(url)
+        .set('Authorization', `Bearer ${getAdminUserToken()}`)
+        .send({})
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'body required for request');
+          done();
+        });
+    });
 
-  it('should NOT register a new user when missing password', (done) => {
-    const userMissingPassword = Object.assign({}, newUser);
-    delete userMissingPassword.password;
-    chai
-      .request(server)
-      .post(url)
-      .set('Authorization', `Bearer ${getAdminUserToken()}`)
-      .send(userMissingPassword)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.message.should.be.a('string');
-        res.body.stack.should.be.a('string');
-        res.body.message.should.eql('Password is empty');
-        done();
-      });
+    it('should NOT register a new user when missing name', (done) => {
+      const registerUser = Object.assign({}, newUser);
+      delete registerUser.name;
+      chai
+        .request(server)
+        .post(url)
+        .set('Authorization', `Bearer ${getAdminUserToken()}`)
+        .send(registerUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'name is empty');
+          done();
+        });
+    });
+
+    it('should NOT register a new user when name is not a string', (done) => {
+      const registerUser = Object.assign({}, newUser);
+      delete registerUser.name;
+      registerUser.name = 1;
+      chai
+        .request(server)
+        .post(url)
+        .set('Authorization', `Bearer ${getAdminUserToken()}`)
+        .send(registerUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'name must be a string');
+          done();
+        });
+    });
+
+    it('should NOT register a new user when missing email', (done) => {
+      const registerUser = Object.assign({}, newUser);
+      delete registerUser.email;
+      chai
+        .request(server)
+        .post(url)
+        .set('Authorization', `Bearer ${getAdminUserToken()}`)
+        .send(registerUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'email is empty');
+          done();
+        });
+    });
+
+    it('should NOT register a new user when email is not a string', (done) => {
+      const registerUser = Object.assign({}, newUser);
+      delete registerUser.email;
+      registerUser.email = 1;
+      chai
+        .request(server)
+        .post(url)
+        .set('Authorization', `Bearer ${getAdminUserToken()}`)
+        .send(registerUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'email must be a string');
+          done();
+        });
+    });
+
+    it('should NOT register a new user when email is not correctly formatted', () => {});
+
+    it('should NOT register a new user when missing password', (done) => {
+      const registerUser = Object.assign({}, newUser);
+      delete registerUser.password;
+      chai
+        .request(server)
+        .post(url)
+        .set('Authorization', `Bearer ${getAdminUserToken()}`)
+        .send(registerUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'password is empty');
+          done();
+        });
+    });
+
+    it('should NOT register a new user when password is not a string', (done) => {
+      const registerUser = Object.assign({}, newUser);
+      delete registerUser.password;
+      registerUser.password = 1;
+      chai
+        .request(server)
+        .post(url)
+        .set('Authorization', `Bearer ${getAdminUserToken()}`)
+        .send(registerUser)
+        .end((err, res) => {
+          res.should.have.status(400);
+          checkValidationResponse(res.body, 'password must be a string');
+          done();
+        });
+    });
+
+    it(`should NOT register a new user when password does not contain a number`, () => {});
+
+    it(`should NOT register a new user when password does not contain a symbol`, () => {});
   });
 });
