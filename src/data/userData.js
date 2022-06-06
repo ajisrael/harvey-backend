72e3a0f246bfe3d9d0afaf18aefaa3f8420cd35c
ajisrael <ajisrael@mtu.edu>
2022-06-06 12:56:30 -0400

    r   - changed password requirements to minimum of 8 chars and max of 32

--- src/data/userData.js
+++ src/data/userData.js
@@ -2,25 +2,25 @@ const users = [
   {
     name: 'Admin User',
     email: 'admin@example.com',
-    password: '1234567',
+    password: '1234567!',
     isAdmin: 1,
   },
   {
     name: 'John Doe',
     email: 'john@example.com',
-    password: '1234567',
+    password: '1234567!',
     isAdmin: 0,
   },
   {
     name: 'Jane Doe',
     email: 'jane@example.com',
-    password: '1234567',
+    password: '1234567!',
     isAdmin: 0,
   },
   {
     name: 'Katelyn Israels',
     email: 'katelyn@example.com',
-    password: '1234567',
+    password: '1234567!',
     isAdmin: 1,
   },
 ];
