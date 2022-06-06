d61527c147601b86266f1781c8ccf6beaa8cec66
ajisrael <ajisrael@mtu.edu>
2022-06-06 13:16:57 -0400

    r   - created sub functions for attribute validation and added messages handler to fns

--- src/middleware/actionsValidator.js
+++ src/middleware/actionsValidator.js
@@ -1,3 +1,5 @@
+import { throwErrorWithMessagesOrCallNext } from './errorMiddleware.js';
+
 function areBedIdsStrings(bedIds) {
   const nonStrings = bedIds.filter((bedId) => {
     return typeof bedId !== 'string';
@@ -5,24 +7,66 @@ function areBedIdsStrings(bedIds) {
   return nonStrings.length === 0;
 }
 
+function validateActionCompleted(actionCompleted, messages) {
+  if (!actionCompleted) {
+    messages.push('actionCompleted is empty');
+  } else if (typeof actionCompleted !== 'number') {
+    messages.push('actionCompleted must be a number');
+  }
+}
+
+function validateActionCompletedType(actionCompletedType, messages) {
+  if (!actionCompletedType) {
+    messages.push('actionCompletedType is empty');
+  } else if (typeof actionCompletedType !== 'string') {
+    messages.push('actionCompletedType must be a string');
+  }
+}
+
+function validateActionId(actionId, messages) {
+  if (!actionId) {
+    messages.push('actionId is empty');
+  } else if (typeof actionId !== 'number') {
+    messages.push('actionId must be a number');
+  }
+}
+
+function validateActionName(actionName, messages) {
+  if (!actionName) {
+    messages.push('actionName is empty');
+  } else if (typeof actionName !== 'string') {
+    messages.push('actionName must be a string');
+  }
+}
+
+function validateActionType(actionType, messages) {
+  if (!actionType) {
+    messages.push('actionType is empty');
+  } else if (typeof actionType !== 'string') {
+    messages.push('actionType must be a string');
+  }
+}
+
+function validateBedIds(bedIds, messages) {
+  if (!bedIds) {
+    messages.push('bedIds is empty');
+  } else if (!Array.isArray(bedIds)) {
+    messages.push('bedIds must be an array');
+  } else if (bedIds.length === 0) {
+    messages.push('bedIds must contain at least one bedId');
+  } else if (!areBedIdsStrings(bedIds)) {
+    messages.push('bedId in bedIds array must be a string');
+  }
+}
+
 function validateActionGet(req, res, next) {
   let messages = [];
 
   if (Object.keys(req.body).length !== 0) {
-    if (!req.body.actionId) {
-      messages.push('actionId is empty');
-    } else if (typeof req.body.actionId !== 'number') {
-      messages.push('actionId must be a number');
-    }
+    validateActionId(req.body.actionId, messages);
   }
 
-  if (messages.length) {
-    let error = new Error(messages.join(', '));
-    res.status(400);
-    throw error;
-  } else {
-    next();
-  }
+  throwErrorWithMessagesOrCallNext(messages, res, next);
 }
 
 function validateActionPost(req, res, next) {
@@ -31,48 +75,14 @@ function validateActionPost(req, res, next) {
   if (Object.keys(req.body).length === 0) {
     messages.push('body required for request');
   } else {
-    if (!req.body.bedIds) {
-      messages.push('bedIds is empty');
-    } else if (!Array.isArray(req.body.bedIds)) {
-      messages.push('bedIds must be an array');
-    } else if (req.body.bedIds.length === 0) {
-      messages.push('bedIds must contain at least one bedId');
-    } else if (!areBedIdsStrings(req.body.bedIds)) {
-      messages.push('bedId in bedIds array must be a string');
-    }
-
-    if (!req.body.actionCompleted) {
-      messages.push('actionCompleted is empty');
-    } else if (typeof req.body.actionCompleted !== 'number') {
-      messages.push('actionCompleted must be a number');
-    }
-
-    if (!req.body.actionCompletedType) {
-      messages.push('actionCompletedType is empty');
-    } else if (typeof req.body.actionCompletedType !== 'string') {
-      messages.push('actionCompletedType must be a string');
-    }
-
-    if (!req.body.actionName) {
-      messages.push('actionName is empty');
-    } else if (typeof req.body.actionName !== 'string') {
-      messages.push('actionName must be a string');
-    }
-
-    if (!req.body.actionType) {
-      messages.push('actionType is empty');
-    } else if (typeof req.body.actionType !== 'string') {
-      messages.push('actionType must be a string');
-    }
+    validateActionCompleted(req.body.actionCompleted, messages);
+    validateActionCompletedType(req.body.actionCompletedType, messages);
+    validateActionName(req.body.actionName, messages);
+    validateActionType(req.body.actionType, messages);
+    validateBedIds(req.body.bedIds, messages);
   }
 
-  if (messages.length) {
-    let error = new Error(messages.join(', '));
-    res.status(400);
-    throw error;
-  } else {
-    next();
-  }
+  throwErrorWithMessagesOrCallNext(messages, res, next);
 }
 
 export { validateActionGet, validateActionPost };
